15a1683c146b2e6fa841b48e6b8144e8c965a45e
Chaitanya <chaitanyapandit@me.com>
2017-08-24 15:56:49 +0530

    Initial Commit

---
+++ Checkout/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Checkout
+//
+//  Created by Chaitanya Pandit on 24/08/17.
+//  Copyright © 2017 Chaitanya Pandit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
